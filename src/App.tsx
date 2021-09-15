19920d5903bc08e2b50e2307be6e83b4803bb955
Pedro Takahashi <pedroh.takahashi@gmail.com>
2021-09-15 16:27:31 -0300

    feat(base): base of project ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import { GlobalStyle } from "./assets/styles/global";
 export function App() {
   return (
     <div className="App">
       <h2> Hello world</h2>
+      <GlobalStyle />
     </div>
   );
 }
